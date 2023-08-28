ba18ec02953e8f016f9f0ae7251734b7cd8dbb79
Binvie <thien97.night1@gmail.com>
2023-08-28 21:10:09 +0700

    ok

--- Module3/ss10/exercise/cart/src/main/java/com/example/cart/model/Cart.java
+++ Module3/ss10/exercise/cart/src/main/java/com/example/cart/model/Cart.java
@@ -10,52 +10,73 @@ import java.util.Map;
 
 @Repository
 public class Cart {
-    private Map<Product, Integer> productMap = new HashMap<>();
+    private Map<Product,Integer> products = new HashMap<>();
 
     public Cart() {
     }
 
-    public Cart(Map<Product, Integer> productMap) {
-        this.productMap = productMap;
+    public Cart(Map<Product,Integer> products) {
+        this.products = products;
     }
 
-    private Boolean checkItemInCart(Product product) {
-        for (Map.Entry<Product, Integer> entry : productMap.entrySet()) {
-            if (entry.getKey().equals(product.getId())) {
+    public Map<Product,Integer> getProducts() {
+        return products;
+    }
+
+    private boolean checkItemInCart(Product product){
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+//            if(entry.getKey().getId().equals(product.getId())){
+                if (entry.getKey().getId()==product.getId()){
                 return true;
             }
         }
         return false;
     }
 
-    public Map.Entry<Product, Integer> selectItemInCart(Product product) {
-        for (Map.Entry<Product, Integer> entry : productMap.entrySet()) {
-            if (entry.getKey().equals(product.getId())) {
+    private Map.Entry<Product, Integer> selectItemInCart(Product product){
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            if(entry.getKey().getId() == product.getId()){
                 return entry;
             }
         }
         return null;
     }
 
-    public void addProduct(Product product) {
-        if (Boolean.FALSE.equals(checkItemInCart(product))) {
-            productMap.put(product, 1);
+    public void addProduct(Product product){
+        if (!checkItemInCart(product)){
+            products.put(product,1);
         } else {
-            Map.Entry<Product, Integer> item = selectItemInCart(product);
-            Integer amount = item.getValue() + 1;
-            productMap.replace(item.getKey(), amount);
+            Map.Entry<Product, Integer> itemEntry = selectItemInCart(product);
+            Integer newQuantity = itemEntry.getValue() + 1;
+            products.replace(itemEntry.getKey(),newQuantity);
         }
     }
-    public Integer countProductQuantity(){
-        Integer quantity = 0;
-        for (Map.Entry<Product,Integer> entry : productMap.entrySet()){
-            quantity += entry.getValue();
-        }return quantity;
+    public void subProduct(Product product){
+        Map.Entry<Product,Integer> integerEntry =selectItemInCart(product);
+        Integer newQuantity = integerEntry.getValue()-1;
+        products.replace(integerEntry.getKey(),newQuantity);
     }
-    public Float totalPayment(){
+
+    public Integer countProductQuantity(){
+        Integer productQuantity = 0;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            productQuantity += entry.getValue();
+        }
+        return productQuantity;
+    }
+
+    public Integer countItemQuantity(){
+        return products.size();
+    }
+
+    public Float countTotalPayment(){
         float payment = 0;
-        for (Map.Entry<Product,Integer> entry : productMap.entrySet()){
-            payment += (float) (entry.getValue() * entry.getKey().getPrice());
-        }return  payment;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            payment += entry.getKey().getPrice() * (float) entry.getValue();
+        }
+        return payment;
+    }
+    public void clearCart(){
+        products.clear();
     }
 }
--- Module3/ss10/exercise/cart/src/main/java/com/example/cart/repository/IProductRepository.java
+++ Module3/ss10/exercise/cart/src/main/java/com/example/cart/repository/IProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.cart.repository;
 
-public interface IProductRepository {
+import com.example.cart.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductRepository extends JpaRepository<Product,Integer> {
 }
